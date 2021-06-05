be2d6fbd8d0e17214ae3785dc92069c6b6befe6f
clare0901 <clarerebello0901@gmail.com>
2021-06-05 17:23:02 +0530

    changes

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,7 +4,7 @@ import "../App.scss";
 function Nav() {
     return(
         <div className="navbar-div">
-            <h1 className="navbar">Hola !!</h1>
+            <h1 className="navbar">&gt;.&lt;</h1>
         </div>
     )
 }
